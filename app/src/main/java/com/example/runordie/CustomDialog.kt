c147f491bc97ff0d31b922db8c983d1017b311fb
eason Cheng <easonchean@foxmail.com>
2023-03-09 22:45:08 +0800

    Version1.0----分组功能

---
+++ app/src/main/java/com/example/runordie/CustomDialog.kt
@@ -0,0 +1,138 @@
+package com.example.runordie
+
+import android.app.Dialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.OrientationEventListener
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class CustomDialog : Dialog { //继承Dialog类
+    constructor(context: Context):super(context){}  //调用父类的构造方法
+    constructor(context:Context, theme:Int):super(context,theme){}
+
+    class Builder(context: Context){ //Bulider模式构建
+        private var title:String? = null
+        private var message:String?= null
+        private var contentView:View? = null
+        private var positiveButtonText: String? = null
+        private var negativeButtonText:String? = null
+        private var singleButtonText:String? = null
+        private var positiveButtonClickListener:View.OnClickListener? = null
+        private var negativeButtonClickListener:View.OnClickListener? = null
+        private var singleButtonClickListener:View.OnClickListener? = null
+
+        private val layout: View
+        private val dialog:CustomDialog = CustomDialog(context,R.style.CustomDialog)
+
+
+        init{
+            //这里传入自定义的style，直接影响此Dialog的显示效果
+            //TODO:layoutInflater是什么
+            var inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            layout = inflater.inflate(R.layout.custom_dialog,null)
+
+            dialog.addContentView(layout,ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT))
+        }
+
+        fun setTitle(title:String) :Builder {
+            this.title = title
+            return this
+        }
+
+        fun setMessage(message:String):Builder {
+            this.message = message
+            return this
+        }
+
+        fun setContentView(v:View):Builder {
+            this.contentView = v
+            return this
+        }
+
+        fun setPositiveButton(positiveButtonText:String,listener: View.OnClickListener):Builder {
+            this.positiveButtonText = positiveButtonText
+            this.positiveButtonClickListener = listener
+            return this
+        }
+
+        fun setNegativeButton(negativeButtonText: String, listener: View.OnClickListener):Builder {
+            this.negativeButtonText = negativeButtonText
+            this.negativeButtonClickListener = listener
+            return this
+        }
+
+        fun setSingleButton(singleButtonText:String, listener: View.OnClickListener):Builder {
+            this.singleButtonText = singleButtonText
+            this.singleButtonClickListener = listener
+            return this
+        }
+
+        //创建单按钮对话框
+        fun createSingleButtonDialog():CustomDialog {
+            showSingleButton()
+            layout.findViewById<View>(R.id.singleBtn).setOnClickListener(singleButtonClickListener)
+            if(singleButtonText != null) {
+                (layout.findViewById<View>(R.id.singleBtn) as TextView).text = singleButtonText
+            } else{
+                (layout.findViewById<View>(R.id.singleBtn) as TextView).text = "知道了"
+            }
+            create()
+            return dialog
+        }
+
+        fun createTwoButtonDialog(): CustomDialog {
+            showTwoButton()
+            layout.findViewById<View>(R.id.positiveBtn).setOnClickListener(positiveButtonClickListener)
+            layout.findViewById<View>(R.id.negativeBtn).setOnClickListener(negativeButtonClickListener)
+            //如果传入的按钮文字为空，则使用默认的“确定”和“取消”
+            if (positiveButtonText != null) {
+                (layout.findViewById<View>(R.id.positiveBtn) as TextView).text = positiveButtonText
+            } else {
+                (layout.findViewById<View>(R.id.positiveBtn) as TextView).text = "确定"
+            }
+            if (negativeButtonText != null) {
+                (layout.findViewById<View>(R.id.negativeBtn) as TextView).text = negativeButtonText
+            } else {
+                (layout.findViewById<View>(R.id.negativeBtn) as TextView).text = "取消"
+            }
+            create()
+            return dialog
+        }
+
+        private fun create() {
+            if (message != null) {      //设置提示内容
+                (layout.findViewById<View>(R.id.message) as TextView).text = message
+            } else if (contentView != null) {       //如果使用Builder的setContentview()方法传入了布局，则使用传入的布局
+                (layout.findViewById<View>(R.id.content) as LinearLayout).removeAllViews() //首先删除所有布局
+                (layout.findViewById<View>(R.id.content) as LinearLayout) //线性的页面，后一个参数是和父页面一样大小
+                    .addView(contentView, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+            }
+            if(title!=null && title!!.isNotEmpty()){
+                (layout.findViewById<View>(R.id.title) as TextView).text = title
+                showTitle()
+            }
+            dialog.setContentView(layout)
+            dialog.setCancelable(true)     //用户可以点击手机Back键取消对话框显示
+            dialog.setCanceledOnTouchOutside(false)        //用户不能通过点击对话框之外的地方取消对话框显示
+        }
+
+        //显示双按钮布局，隐藏单按钮
+        private fun showTwoButton(){
+            layout.findViewById<View>(R.id.singleButtonLayout).visibility = View.GONE
+            layout.findViewById<View>(R.id.twoButtonLayout).visibility = View.VISIBLE
+        }
+
+        // 显示单按钮布局，隐藏双按钮
+        private fun showSingleButton() {
+            layout.findViewById<View>(R.id.singleButtonLayout).visibility = View.VISIBLE
+            layout.findViewById<View>(R.id.twoButtonLayout).visibility = View.GONE
+        }
+
+        private fun showTitle() {
+            layout.findViewById<View>(R.id.title).visibility = View.VISIBLE
+        }
+    }
+}
